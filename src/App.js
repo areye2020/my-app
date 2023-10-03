85bcf43ed60f5811b70ad6891243a001c07fa52f
Adriana Reyes <areye2020@gmail.com>
2023-10-02 23:35:30 -0500

    working multipage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React from "react"
-import Header from "./Components/Head/Header"
-import Features from "./Components/Features/Features"
-import Home from "./Components/Hero/Home"
-import Portfolio from "./Components/Portfolio/Portfolio"
-import Resume from "./Components/Resume/Resume"
-import Testimonial from "./Components/Testimonial/Testimonial"
-import Blog from "./Components/Blog/Blog"
-import Contact from "./Components/Contact/Contact"
-import Footer from "./Components/Footer"
-import "./App.css"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Head/Header";
+import Features from "./Components/Features/Features";
+import Home from "./Components/Hero/Home";
+import Resume from "./Components/Resume/Resume";
+import Blog from "./Components/Blog/Blog";
+import Contact from "./Components/Contact/Contact";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonial from "./Components/Testimonial/Testimonial";
+import Footer from "./Components/Footer";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
-      <Features />
-      <Resume />
-      <Blog/>
-      {/* <Portfolio /> */}
-      <Testimonial />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        {/* Add more routes for other pages as needed */}
+      </Routes>
       <Footer />
-    </>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/Components/Head/Header.js
+++ src/Components/Head/Header.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import "./header.css"
 import logo from "../pic/logo.png"
+import { Link } from "react-router-dom";
+
 
 const Header = () => {
   // fixed Header
@@ -9,50 +11,62 @@ const Header = () => {
     header.classList.toggle("active", window.scrollY > 100)
   })
   // Toogle Menu
-  const [Mobile, setMobile] = useState(false)
+  const [mobile, setMobile] = useState(false);
+
+  const closeMobileMenu = () => {
+    setMobile(false);
+  };
+
   return (
     <>
-      <header className='header'>
-        <div className='container d_flex'>
-          <div className='logo'>
-            <img alt='' />
-          </div>
-
-          <div className='navlink'>
-            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
-              {/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
-              <li>
-                <a href='#home'>home</a>
-              </li>
-              <li>
-                <a href='#features'>technologies</a>
-              </li>
-              {/* <li>
-                <a href='#portfolio'>portfolio</a>
-              </li> */}
-              <li>
-                <a href='#resume'>resume</a>
-              </li>
-              <li>
-                <a href='#clients'>blog</a>
-              </li>
-              <li>
-                <a href='#blog'>portfolio</a>
-              </li>
-              <li>
-                <a href='#contact'>contact</a>
-              </li>
-              {/* <li>
-                <button className='home-btn'>BUY NOW</button>
-              </li> */}
-            </ul>
-
-            <button className='toggle' onClick={() => setMobile(!Mobile)}>
-              {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
-            </button>
-          </div>
+    <header className="header">
+      <div className="container d_flex">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
         </div>
-      </header>
+
+        <div className="navlink">
+          <ul className={mobile ? "nav-links-mobile" : "link f_flex uppercase"}>
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/features" onClick={closeMobileMenu}>
+                Technologies
+              </Link>
+            </li>
+            <li>
+              <Link to="/portfolio" onClick={closeMobileMenu}>
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="/resume" onClick={closeMobileMenu}>
+                Resume
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+            {/* Add more navigation links for other pages */}
+          </ul>
+
+          <button className="toggle" onClick={() => setMobile(!mobile)}>
+            {mobile ? (
+              <i className="fas fa-times close home-btn"></i>
+            ) : (
+              <i className="fas fa-bars open"></i>
+            )}
+          </button>
+        </div>
+      </div>
+    </header>
     </>
   )
 }
