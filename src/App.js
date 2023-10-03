a4ddcbeca5b0d6051f05ad44f7835db0ac17710e
Adriana Reyes <areye2020@gmail.com>
2023-10-02 22:56:04 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,60 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Header from "./Components/Head/Header";
-import Features from "./Components/Features/forHome";
+import Features from "./Components/Features/Features";
 import Home from "./Components/Hero/Home";
-import Portfolio from "./Components/Portfolio/Portfolio";
 import Resume from "./Components/Resume/Resume";
-import Testimonial from "./Components/Testimonial/Testimonial";
 import Blog from "./Components/Blog/Blog";
+import Testimonial from "./Components/Testimonial/Testimonial";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer";
-import Journey from "./Components/Testimonial/journey";
 import "./App.css";
-import "./Components/Head/header.css";
 
-function App() {
-  // fixed Header
-  window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header");
-    header.classList.toggle("active", window.scrollY > 100);
-  });
-
-  // Toggle Menu
-  const [Mobile, setMobile] = useState(false);
-
-  // State to manage the current page
-  const [currentPage, setCurrentPage] = useState("home");
-
-  // Function to change the current page
-  const changePage = (pageName) => {
-    setCurrentPage(pageName);
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="header">
-        <div className="container d_flex">
-          <div className="logo">
-            <span className="name">Adriana Reyes</span>
-            <img alt="" />
-          </div>
-          <ul>
-            <li onClick={() => changePage("home")}>Home</li>
-            <li onClick={() => changePage("journey")}>Personal Journey</li>
-            <li onClick={() => changePage("resume")}>Resume</li>
-            {/* Add more navigation items here */}
-            <li onClick={() => changePage("contact")}>Contact</li>
-          </ul>
-        </div>
-      </header>
-      {/* Conditionally render components based on the current page */}
-      {currentPage === "home" && <Home />}
-      {currentPage === "journey" && <Journey />}
-      {currentPage === "resume" && <Resume />}
-      {/* Add more conditionals for other components */}
-      {currentPage === "contact" && <Contact />}
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/blog" element={<Blog />} />
+        {/* Add more routes for other pages as needed */}
+      </Routes>
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
--- src/Components/Head/Header.js
+++ src/Components/Head/Header.js
@@ -1,76 +1,56 @@
-import React, { useState } from "react"
-import "./header.css"
-import logo from "../pic/logo.png"
-import Journey from "../Testimonial/journey"
-import Home from "../Hero/Home"
-import Resume from "../Resume/forHome"
-import Contact from "../Contact/Contact"
+import React, { useState, useEffect } from "react";
+import "./header.css";
+import logo from "../pic/logo.png";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
-  // fixed Header
-  window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header")
-    header.classList.toggle("active", window.scrollY > 100)
-  })
-  // Toogle Menu
-  const [Mobile, setMobile] = useState(false)
+  const location = useLocation();
+  const [mobile, setMobile] = useState(false);
 
-  // State to manage the current page
-  const [currentPage, setCurrentPage] = useState("home");
+  useEffect(() => {
+    const header = document.querySelector(".header");
+    header.classList.toggle("active", window.scrollY > 100);
+  }, []);
 
-  // Function to change the current page
-  const changePage = (pageName) => {
-    setCurrentPage(pageName);
+  const closeMobileMenu = () => {
+    setMobile(false);
   };
 
   return (
-    <>
-      <header className='header'>
-        <div className='container d_flex'>
-          <div className='logo'>
-            <img alt='' />
-          </div>
-
-          <div className='navlink'>
-            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
-              {/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
-              <li onClick={() => changePage("home")}>Home</li>
-              <li onClick={() => changePage("journey")}>Personal Journey</li>
-              <li onClick={() => changePage("resume")}>Resume</li>
-              <li onClick={() => changePage("contact")}>Contact</li>
-              {/* <li>
-                <a href='#portfolio'>portfolio</a>
-              </li> */}
-              {/* <li>
-                <a href='#resume'>resume</a>
-              </li>
-              <li>
-                <a href='#clients'>blog</a>
-              </li>
-              <li>
-                <a href='#blog'>portfolio</a>
-              </li>
-              <li>
-                <a href='#contact'>contact</a>
-              </li> */}
-              {/* <li>
-                <button className='home-btn'>BUY NOW</button>
-              </li> */}
-            </ul>
-
-            <button className='toggle' onClick={() => setMobile(!Mobile)}>
-              {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
-            </button>
-          </div>
+    <header className="header">
+      <div className="container d_flex">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
         </div>
-      </header>
-      {currentPage === "home" && <Home />}
-      {currentPage === "journey" && <Journey />}
-      {currentPage === "resume" && <Resume />}
-      {/* Add more conditionals for other components */}
-      {currentPage === "contact" && <Contact />}
-    </>
-  )
-}
 
-export default Header
+        <div className="navlink">
+          <ul className={mobile ? "nav-links-mobile" : "link f_flex uppercase"}>
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/features" onClick={closeMobileMenu}>
+                Technologies
+              </Link>
+            </li>
+            {/* Add more navigation links for other pages */}
+          </ul>
+
+          <button className="toggle" onClick={() => setMobile(!mobile)}>
+            {mobile ? (
+              <i className="fas fa-times close home-btn"></i>
+            ) : (
+              <i className="fas fa-bars open"></i>
+            )}
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
