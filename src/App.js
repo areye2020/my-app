9999a2518525bbe472539e56c50b1a60da237535
areye2020 <43623753+areye2020@users.noreply.github.com>
2024-05-07 00:45:52 -0500

    think its working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Head/Header";
+import Features from "./Components/Features/Features";
 import Home from "./Components/Hero/Home";
 import Resume from "./Components/Resume/Resume";
+import Blog from "./Components/Blog/Blog";
 import Contact from "./Components/Contact/Contact";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonial from "./Components/Testimonial/Testimonial";
 import Journey from "./Components/Testimonial/journey";
 import Footer from "./Components/Footer";
 import "./App.css";
@@ -13,13 +17,11 @@ const App = () => {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/journey" element={<Journey />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/contact" element={<Contact />} />
         {/* Add more routes for other pages as needed */}
-        {/* Default route to HOME */}
-        <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
     </Router>
